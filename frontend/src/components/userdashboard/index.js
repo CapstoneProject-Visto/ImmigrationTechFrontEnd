2c95dcd4ddac39524fa478c29c5fe7b2928b843a
Sachin796 <sachinjaveri.2010@gmail.com>
2020-04-10 03:39:35 -0400

    when no IELTS score landing page added

--- frontend/src/components/userdashboard/index.js
+++ frontend/src/components/userdashboard/index.js
@@ -1,8 +1,33 @@
 import React from "react";
 import RightComponent from "./rightComponent";
+import UserDashboardWithoutIELTS from "./noieltsdashboard";
 import { Container, Row, Col } from "react-bootstrap";
 import { withRouter } from "react-router-dom";
 class UserDashboardMainPage extends React.Component {
+  state = {
+    forward: "",
+  };
+  componentDidMount() {
+    let data = {
+      email: "sachinjav@gmail.com",
+      password: "12345",
+      user_type: "user",
+    };
+    fetch("http://localhost:5001/api/misc/isFirstTime", data)
+      .then((res) => res.json())
+      .then((fdata) => {
+        if (fdata.status == "0") {
+          this.setState({
+            forward: "false",
+          });
+        } else if (fdata.status == "1") {
+          this.setState({
+            forward: "true",
+          });
+        }
+      });
+  }
+
   render() {
     return (
       <>
@@ -15,7 +40,15 @@ class UserDashboardMainPage extends React.Component {
           <Container>
             <Row>
               <Col>
-                <RightComponent />
+                {this.state.forward != ""
+                  ? [
+                      this.state.forward == "true" ? (
+                        <RightComponent />
+                      ) : (
+                        <UserDashboardWithoutIELTS />
+                      ),
+                    ]
+                  : null}
               </Col>
             </Row>
           </Container>
